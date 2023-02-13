05845d7dc70e98855bc16f91ac9b9ff44b2b4630
Yoyisn <teodork789@gmail.com>
2023-02-13 15:52:01 -0500

    Copia Pri

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ function CalcularDefinitiva() {
     let num2 = document.getElementById('n2').value;
     let num3 = document.getElementById('n3').value; 
     let definitive = (parseFloat(num1) + parseFloat(num2) + parseFloat(num3)) / 3;
-    document.getElementById('NotaDefinitive').innerHTML = definitive; //El 'innerHTML = definitive' funciona para
-    //traer el valor de definitive y ponerlo en pantalla
+    document.getElementById('NotaDefinitive').innerHTML = 'NOTA DEFINITIVA : ' + definitive;  //El 'innerHTML = definitive' funciona para //traer el valor de definitive y ponerlo en pantalla en el html con el id: NotaDefinitive
+    let lack = definitive * 5.0;
+    document.getElementById('lack').innerHTML = 'PARA RECUPERAR : ' + lack;                                                                                           
 };
