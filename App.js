7ba32d52e268f67d024864b75aa211d9d642fdcd
Yoyisn <teodork789@gmail.com>
2023-02-13 17:17:55 -0500

    LO QUE ALCANCE HASTA LA CLASE

--- App.js
+++ App.js
@@ -3,7 +3,16 @@ function CalcularDefinitiva() {
     let num2 = document.getElementById('n2').value;
     let num3 = document.getElementById('n3').value; 
     let definitive = (parseFloat(num1) + parseFloat(num2) + parseFloat(num3)) / 3;
-    document.getElementById('NotaDefinitive').innerHTML = 'NOTA DEFINITIVA : ' + definitive;  //El 'innerHTML = definitive' funciona para //traer el valor de definitive y ponerlo en pantalla en el html con el id: NotaDefinitive
+  //El 'innerHTML = definitive' funciona para //traer el valor de definitive y ponerlo en pantalla en el html con el id: NotaDefinitive
     let lack = definitive * 5.0;
-    document.getElementById('lack').innerHTML = 'PARA RECUPERAR : ' + lack;                                                                                           
+    
+    if (definitive <= 2.0) {
+        document.getElementById('NotaDefinitive').innerHTML =  'DEFINITIVA, PERDIDA : ' + definitive;
+    } else if ( definitive => 2.1 && definitive <= 3.4) {
+        document.getElementById('NotaDefinitive').innerHTML = 'PIERDE, PARA RECUPERAR : ' + lack + ' DEFINITIVA : ' + definitive;
+    }  else if ( definitive => 3.5 && definitive <= 4.5) {
+        document.getElementById('NotaDefinitive').innerHTML = 'APROBO LA MATERIA : ' + definitive;
+    } else if (definitive => 4.6 && definitive <= 5.0) {
+        document.getElementById('NotaDefinitive').innerHTML = 'APROBO LA MATERIA CON HONORES, DEIFNITIVA : ' + definitive;
+    }
 };
